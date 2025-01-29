38ad156c4f74e5ae30a51dfeb78cd0464437cff4
Brianbramasta <Brianaldybramasta@gmail.com>
2025-01-29 08:38:13 +0700

    Merge unrelated histories

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    quality: 100,
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+//     quality: 100,
+//   },
+// }
 
-module.exports = nextConfig 
+// module.exports = nextConfig 
